bc52f9bca80f5a5ee582baf4daf5b9dde11fb5a8
Geirmund Simonsen <geirmund.simonsen@gmail.com>
2018-05-20 19:35:56 +0200

    more commits

--- src/audio/audio.js
+++ src/audio/audio.js
@@ -1,9 +1,14 @@
 window.ctx = new AudioContext()
 console.log('audiocontext is active.')
 
+
+ctx.audioWorklet.addModule('bypass-processor.js').then(() => {
+  let bypassNode = new AudioWorkletNode(ctx, 'bypass-processor');
+});
+
 class Clock {
-    constructor(ctx, lookaheadInSec) {
-        this.bpm = 120;
+    constructor(ctx, lookaheadInSec, bpm) {
+        this.bpm = bpm || 120;
         this.calculateBeats2s();
         this.updateLookahead(lookaheadInSec);
         this.prevT = ctx.currentTime;
@@ -57,7 +62,8 @@ class Clock {
                 keysToBeDeleted.push(nextEntry[0])
                 let f = nextEntry[1][0]
                 let args = nextEntry[1][1]
-                f(this.calculateCtxTimeFromBeat(nextBeat), nextBeat, this, args);
+                args.t = this.calculateCtxTimeFromBeat(nextBeat)
+                f(args);
             } else {
                 break;
             }
@@ -78,7 +84,36 @@ class Clock {
     }
 
     addFunction(beats, func, args) {
+        if (args.fnId === undefined) {
+            args.fnId = Math.random()
+        }
+        args.beats = beats
+        args.clock = this
+        args.ctx = this.ctx
         this.scheduledFuncs.set([beats, Math.random()], [func, args]);
+    }
+
+    stopFunction(fnId) {
+        let entries = this.scheduledFuncs.entries();
+        let keysToBeDeleted = [];
+        while (true) {
+            let nextEntry = entries.next().value;
+            if (nextEntry === undefined) {
+                break;
+            } 
+            
+            let nextBeat = nextEntry[0][0];
+            if (nextEntry[1][1].fnId === fnId) {
+                keysToBeDeleted.push(nextEntry[0])
+                if (nextEntry[1][1].stop) {
+                    let f = nextEntry[1][0]
+                    let args = nextEntry[1][1]
+                    args.t = this.ctx.currentTime
+                    f(args)
+                }
+            }
+        }
+        keysToBeDeleted.forEach((key) => this.scheduledFuncs.delete(key));
     }
 
     getNextBeat() {
@@ -86,8 +121,68 @@ class Clock {
     }
 }
 
-window.makeClock = () => {
-    return new Clock(ctx, 0.1);
+window.makeClock = (bpm) => {
+    return new Clock(ctx, 0.1, bpm);
+}
+
+window.p2f = (pitch) => {
+    return 440 * Math.pow(2, (pitch-69) / 12);
+}
+
+window.powerShaperArray = (power, points) => {
+    var array = [];
+    var spacing = 2 / points;
+    for (let n = -1; n <= 1; n += spacing) {
+        if (n < 0) {
+            array.push(-Math.pow(n, power))
+        } else {
+            array.push(Math.pow(n, power))
+        }
+    }
+    return new Float32Array(array)
+}
+
+window.shaperHexagonic = powerShaperArray(10, 1024);
+
+window.pulsar = (args) => {
+    let freq = p2f(args.pitch) || 440
+
+    let triangleLFO = args.ctx.createOscillator()
+    triangleLFO.type = "triangle"
+    triangleLFO.frequency.value = 1
+
+    let waveshaper = args.ctx.createWaveShaper()
+    waveshaper.curve = shaperHexagonic
+
+    let lfogain = args.ctx.createGain()
+    lfogain.gain.value = freq * 0.2
+
+    let saw1 = args.ctx.createOscillator()
+    saw1.type = 'sawtooth'
+    saw1.frequency.value = freq
+
+    let saw2 = args.ctx.createOscillator()
+    saw2.type = 'sawtooth'
+    saw2.frequency.value = freq
+
+    let amp = args.ctx.createGain()
+    amp.gain.value = 0.2
+
+    triangleLFO.connect(waveshaper)
+    waveshaper.connect(lfogain)
+    lfogain.connect(saw1.frequency)
+    saw1.connect(amp)
+    saw2.connect(amp)
+    amp.connect(args.ctx.destination)
+
+    saw1.start(args.t)
+    saw2.start(args.t)
+    triangleLFO.start(args.t)
+
+    args.clock.addFunction(args.beats + 4, () => {
+        saw1.stop(args.t);
+        saw2.stop(args.t);
+    }, args)
 }
 
 window.telephone = (t, beats, clock, count) => {
